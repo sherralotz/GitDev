eaf3b6e6480c4dda68ab0fa34c1226a0c790dc06
sherralotz <54898389+sherralotz@users.noreply.github.com>
2021-07-23 22:50:53 +0800

    Added datatable js as an plugin for integrating with tables efficiently

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
- 
 $(document).ready( function () {
-    console.log("hello world");
+    getGitJSONData();
 } ); 
+
+function getGitJSONData(){  
+    getJSON("https://api.github.com/repositories/19438/commits").then(response => { 
+        $('#table_id').DataTable({
+            data: response, 
+            columns: [
+                { data: "sha", title: "Column1" },
+                { data: "url", title: "Column2" },
+            ]
+        }); 
+    });
+}
+
+// -------- REUSABLE FUNCTIONS --------
+async function getJSON(url){
+    response = await fetch(url);
+    return response.json();
+}
  
